72b7c16943839136fc03899d36043ee62137b542
hbk01 <3243430237@qq.com>
2020-11-21 20:21:16 +0800

    新增版本信息

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -20,25 +21,42 @@ type Config struct { // {{{
 	PM     []PackageManager `json:"list"`
 } // }}}
 
+var (
+	Debug      = true
+	Version    = "v0.1"
+	ConfigFile = "./config.json"
+)
+
 func main() {
 	if len(os.Args) == 1 {
 		// mcn
-		fmt.Println("功能：根据配置文件修改镜像源地址")
-		fmt.Println("USAGE:")
-		fmt.Println("\tmcn [ [ --update ] | [ [--config=FILE] PackageManagerTag ] ]")
-		fmt.Println("")
-		fmt.Println("EXAMPLE:")
-		fmt.Println("\tmcn go # 修改go的源，默认使用与mcn命令同目录的配置文件")
-		fmt.Println("\tmcn --config=\"$HOME/MirrorCN.json\" pacman # 使用指定的配置文件修改pacman的源")
+		fmt.Println("功能:")
+		fmt.Println("\t根据配置文件修改镜像源地址")
+		fmt.Println()
+		fmt.Println("版本:")
+		fmt.Println("\t发行版: " + strconv.FormatBool(!Debug))
+		fmt.Println("\t版本名: " + Version)
+		fmt.Println("\t项目地址: " + "https://github.com/hbk01/MirrorCN")
+		fmt.Println()
+		fmt.Println("使用:")
+		fmt.Println("\tmcn [--update] | [[--config=FILE] PackageManagerTag]")
+		fmt.Println()
+		fmt.Println("示例:")
+		fmt.Println("\tmcn pacman apt # 修改pacman和apt的源，默认使用与mcn命令同目录的./config.json")
+		fmt.Println("\tmcn --config=\"$HOME/MirrorCN.json\" pip # 使用指定的配置文件修改pip的源")
 		fmt.Println("\tmcn --update # 下载最新的配置文件")
 	} else if os.Args[1] == "--update" {
 		// mcn --update
 		fmt.Println("Update...")
 		os.Exit(1)
 	} else if strings.HasPrefix(os.Args[1], "--config-file=") {
-		// mcn --config-file="hahaha" apt
-		file := strings.Split(os.Args[1], "=")[1]
-		fmt.Println(file)
+		// mcn --config-file="./MirrorCN.json" apt
+		ConfigFile = strings.Split(os.Args[1], "=")[1]
+		fmt.Println(ConfigFile)
+		pm := getAllPM(os.Args)
+		fmt.Println("You Input The PM(s):", pm)
+	} else {
+		// mcn pacman apt
 		pm := getAllPM(os.Args)
 		fmt.Println("You Input The PM(s):", pm)
 	}
